8f812935f564c3163d4911226729fde7b9868c6e
codebyte156 <rahulraut007123@gmail.com>
2024-05-21 17:51:42 +0530

    init

---
+++ problem78.cpp
@@ -0,0 +1,46 @@
+/*
+Given an integer array nums of unique elements, return all possible 
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+*/
+
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(nums, 0, current, result);
+        return result;
+    }
+    
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+        for (int i = start; i < nums.size(); ++i) {
+            current.push_back(nums[i]);
+            backtrack(nums, i + 1, current, result);
+            current.pop_back();
+        }
+    }
+};
+
+
+//By R2
